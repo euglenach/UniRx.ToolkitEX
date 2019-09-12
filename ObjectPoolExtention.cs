d71b467a58ee667f55730d9378b3ec0fc5a8f505
euglenach <midorimushiiii@gmail.com>
2019-09-12 14:55:11 +0900

    Rentメソッドの拡張メソッドを追加

---
+++ ObjectPoolExtention.cs
@@ -0,0 +1,26 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace UniRx.Toolkit {
+    public static class ObjectPoolExtension {
+        public static T Rent<T>(this ObjectPool<T> pool,Transform parent)where T : UnityEngine.Component{
+            var obj = pool.Rent();
+            obj.transform.parent = parent;
+            return obj;
+        }
+        
+        public static T Rent<T>(this ObjectPool<T> pool,Vector3 position)where T : UnityEngine.Component{
+            var obj = pool.Rent();
+            obj.transform.position = position;
+            return obj;
+        }
+        
+        public static T Rent<T>(this ObjectPool<T> pool,Vector3 position,Quaternion rotation)where T : UnityEngine.Component{
+            var obj = pool.Rent();
+            obj.transform.position = position;
+            obj.transform.rotation = rotation;
+            return obj;
+        }
+
+    }
+}
